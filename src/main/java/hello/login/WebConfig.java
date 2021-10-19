704b8f163a1e55603dfd4b89c5299284850d9bbf
jinsb1995 <jinsb1995@gmail.com>
2021-10-20 00:01:50 +0900

    filter, interceptor

---
+++ src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,36 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        // was를 띄울 때 filter를 같이 넣어준다.
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        // was를 띄울 때 filter를 같이 넣어준다.
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+}
