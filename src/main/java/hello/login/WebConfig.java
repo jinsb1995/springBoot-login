1cac3f664c93fc1905a9120fec049d1288b5626b
jinsb1995 <jinsb1995@gmail.com>
2021-10-23 13:48:40 +0900

    interceptor loginCheckInterceptor

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,17 +13,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer{
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/*.ico", "/error");
+                .excludePathPatterns("/css/**", "/*.ico", "/error");  // 여기에서는 인터셉터를 먹이지 않는다.
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/member/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
     }
 
-//    @Bean
+    //    @Bean
     public FilterRegistrationBean logFilter() {
         // was를 띄울 때 filter를 같이 넣어준다.
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
@@ -34,7 +40,7 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         // was를 띄울 때 filter를 같이 넣어준다.
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
--- src/main/java/hello/login/web/interceptor/LogInterceptor.java
+++ src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -16,7 +16,6 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // parameter에 handler는 controller이다
 
         String requestURI = request.getRequestURI();
         String uuid = UUID.randomUUID().toString();
@@ -26,28 +25,29 @@ public class LogInterceptor implements HandlerInterceptor {
         // @RequestMapping: HandlerMethod
         // 정적 리소스: ResourceHttpRequestHandler
         if (handler instanceof HandlerMethod) {
-            HandlerMethod hm = (HandlerMethod) handler; // 호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+            HandlerMethod hm = (HandlerMethod) handler;  // 호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
         }
 
         log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
-
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        // 예외는 여기서 안 찍히니까 afterCompletion에서 찍어야함
         log.info("postHandle [{}]", modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         String requestURI = request.getRequestURI();
-        Object logId = request.getAttribute(LOG_ID);
+        String logId = (String) request.getAttribute(LOG_ID);
 
         log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+
         if(ex != null) {
-            log.info("afterCompletion error!! ", ex);
+            log.error("afterCompletion error!!", ex);
         }
+
+
     }
 }
